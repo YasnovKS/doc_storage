54de8d1dd28f37952ba44b13b053262444af3941
Яснов Кирилл Сергеевич <YasnovKS@snc.local>
2023-04-25 16:56:41 +0500

    feature: add document edit view

--- storage/docs/urls.py
+++ storage/docs/urls.py
@@ -7,7 +7,11 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('create/', views.CreateDocument.as_view(), name='doc_create'),
     path(
-        'document/<pk>', views.DocumentDetail.as_view(),
+        'document/<pk>/', views.DocumentDetail.as_view(),
         name='doc_detail'
+    ),
+    path(
+        'document/<pk>/edit/', views.EditDocument.as_view(),
+        name='doc_edit'
     ),
 ]
--- storage/docs/views.py
+++ storage/docs/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import redirect
-from django.views.generic import ListView, CreateView, DetailView
-from docs.models import Document
-from docs.forms import PostForm
-from django.urls import reverse_lazy
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required
 import reversion
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from docs.forms import PostForm
+from docs.models import Document
+from reversion.models import Version
+from django.urls import reverse_lazy
 
 
 class IndexView(ListView):
@@ -13,14 +14,24 @@ class IndexView(ListView):
     template_name = 'index.html'
 
 
+class DocumentDetail(DetailView):
+    model = Document
+    template_name = 'doc_detail.html'
+
+    def get(self, request, pk):
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        versions = Version.objects.filter(object_id=pk)
+        version_list = list()
+        for version in versions:
+            version_list.append(version.field_dict)
+        context['versions'] = version_list
+        return self.render_to_response(context)
+
+
 class CreateDocument(CreateView):
     form_class = PostForm
     template_name = 'document_create.html'
-    success_url = reverse_lazy('documents:index')
-
-    @method_decorator(login_required)
-    def get(self, request):
-        return super().get(request)
 
     @method_decorator(login_required)
     def post(self, request):
@@ -30,9 +41,10 @@ class CreateDocument(CreateView):
                 document = form.save(commit=False)
                 document.author = request.user
                 document.save()
-        return redirect('docs:index')
+        return redirect('docs:doc_detail', pk=document.id)
 
 
-class DocumentDetail(DetailView):
+class EditDocument(UpdateView):
     model = Document
-    template_name = 'doc_detail.html'
+    template_name = 'document_create.html'
+    fields = ['title', 'content']
